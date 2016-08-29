033dcfb3d58c72067dd7375a63e6e90b93481476
Kcire6 <erickhernandez1710@gmail.com>
2016-08-28 23:00:37 -0600

    Proyect creation

---
+++ Proyecto1_Compi2_201314408/src/proyecto1_compi2_201314408/Proyecto1_Compi2_201314408.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto1_compi2_201314408;
+
+/**
+ *
+ * @author happy_000
+ */
+public class Proyecto1_Compi2_201314408 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
