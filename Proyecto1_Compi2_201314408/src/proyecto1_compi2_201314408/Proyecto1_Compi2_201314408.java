190bf43caa8925bfc8bfa92095a11ba00eb3fca7
Kcire6 <erickhernandez1710@gmail.com>
2016-09-01 09:17:57 -0600

    Commit 2

--- Proyecto1_Compi2_201314408/src/proyecto1_compi2_201314408/Proyecto1_Compi2_201314408.java
+++ Proyecto1_Compi2_201314408/src/proyecto1_compi2_201314408/Proyecto1_Compi2_201314408.java
@@ -3,19 +3,28 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package proyecto1_compi2_201314408;
 
+package proyecto1_compi2_201314408;
+import compilador.*;
+import static compilador.Calc1i.argStack;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import Editor.*;
 /**
  *
  * @author happy_000
  */
+
 public class Proyecto1_Compi2_201314408 {
 
+    public static void main(String args[]){
+       EditorTexto ET = new EditorTexto();
+       ET.setVisible(true);
+    }
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+
     
 }
